89cb3a2c876e5bc18026e93bb15143a5a196536d
Matt Aimonetti <mattaimonetti@gmail.com>
2016-12-05 22:00:57 -0800

    finish implementing the buffer interface

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # audio
+
+`audio` is a generic Go package designed to define a common
+interface to analyze and/or process audio data.
+
+At the heart of the package is the `Buffer` interface and its implementations:
+
+* `FloatBuffer`
+* `IntBuffer`
+
+Decoders, encoders, processors, analyzers and transformers can be written to
+accept or return these types and share a common interface.
--- audio.go
+++ audio.go
@@ -6,6 +6,7 @@ import (
 )
 
 var (
+	// ErrInvalidBuffer is a generic error returned when trying to read/write to an invalid buffer.
 	ErrInvalidBuffer = errors.New("invalid buffer")
 )
 
@@ -21,28 +22,121 @@ type Format struct {
 	Endianness binary.ByteOrder
 }
 
+// Buffer is the representation of an audio buffer.
 type Buffer interface {
+	// PCMFormat is the format of buffer (describing the buffer content/format).
 	PCMFormat() *Format
-	Clone() Buffer
+	// NumFrames returns the number of frames contained in the buffer.
 	NumFrames() int
+	// AsFloatBuffer returns a float buffer from this buffer.
 	AsFloatBuffer() *FloatBuffer
+	// AsIntBuffer returns an int buffer from this buffer.
 	AsIntBuffer() *IntBuffer
+	// Clone creates a clean clone that can be modified without
+	// changing the source buffer.
+	Clone() Buffer
 }
 
+// FloatBuffer is an audio buffer with its PCM data formatted as float64.
 type FloatBuffer struct {
+	// Format is the representation of the underlying data format
 	Format *Format
-	Data   []float64
+	// Data is the buffer PCM data as floats
+	Data []float64
 }
 
-func (buf *FloatBuffer) PCMFormat() *Format          { return buf.Format }
+// PCMFormat returns the buffer format information.
+func (buf *FloatBuffer) PCMFormat() *Format { return buf.Format }
+
+// AsFloatBuffer implements the Buffer interface and returns itself.
 func (buf *FloatBuffer) AsFloatBuffer() *FloatBuffer { return buf }
 
-type IntBuffer struct {
-	Format *Format
-	Data   []int
+// AsIntBuffer returns a copy of this buffer but with data truncated to Ints.
+func (buf *FloatBuffer) AsIntBuffer() *IntBuffer {
+	newB := &IntBuffer{}
+	newB.Data = make([]int, len(buf.Data))
+	for i := 0; i < len(buf.Data); i++ {
+		newB.Data[i] = int(buf.Data[i])
+	}
+	newB.Format = &Format{
+		NumChannels: buf.Format.NumChannels,
+		SampleRate:  buf.Format.SampleRate,
+		BitDepth:    buf.Format.BitDepth,
+		Endianness:  buf.Format.Endianness,
+	}
+	return newB
 }
 
+// Clone creates a clean clone that can be modified without
+// changing the source buffer.
+func (buf *FloatBuffer) Clone() Buffer {
+	if buf == nil {
+		return nil
+	}
+	newB := &FloatBuffer{}
+	newB.Data = make([]float64, len(buf.Data))
+	copy(newB.Data, buf.Data)
+	newB.Format = &Format{
+		NumChannels: buf.Format.NumChannels,
+		SampleRate:  buf.Format.SampleRate,
+		BitDepth:    buf.Format.BitDepth,
+		Endianness:  buf.Format.Endianness,
+	}
+	return newB
+}
+
+// NumFrames returns the number of frames contained in the buffer.
+func (buf *FloatBuffer) NumFrames() int {
+	if buf == nil || buf.Format == nil {
+		return 0
+	}
+	numChannels := buf.Format.NumChannels
+	if numChannels == 0 {
+		numChannels = 1
+	}
+
+	return len(buf.Data) / numChannels
+}
+
+// IntBuffer is an audio buffer with its PCM data formatted as int.
+type IntBuffer struct {
+	// Format is the representation of the underlying data format
+	Format *Format
+	// Data is the buffer PCM data as ints
+	Data []int
+}
+
+// PCMFormat returns the buffer format information.
 func (buf *IntBuffer) PCMFormat() *Format { return buf.Format }
+
+// AsFloatBuffer returns a copy of this buffer but with data converted to floats.
 func (buf *IntBuffer) AsFloatBuffer() *FloatBuffer {
-	return nil
+	newB := &FloatBuffer{}
+	newB.Data = make([]float64, len(buf.Data))
+	for i := 0; i < len(buf.Data); i++ {
+		newB.Data[i] = float64(buf.Data[i])
+	}
+	newB.Format = &Format{
+		NumChannels: buf.Format.NumChannels,
+		SampleRate:  buf.Format.SampleRate,
+		BitDepth:    buf.Format.BitDepth,
+		Endianness:  buf.Format.Endianness,
+	}
+	return newB
+}
+
+// AsIntBuffer implements the Buffer interface and returns itself.
+func (buf *IntBuffer) AsIntBuffer() *IntBuffer { return buf }
+
+// NumFrames returns the number of frames contained in the buffer.
+func (buf *IntBuffer) NumFrame() int {
+	if buf == nil || buf.Format == nil {
+		return 0
+	}
+	numChannels := buf.Format.NumChannels
+	if numChannels == 0 {
+		numChannels = 1
+	}
+
+	return len(buf.Data) / numChannels
 }
