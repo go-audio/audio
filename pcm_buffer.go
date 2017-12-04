f3b38c427630ab772bf06bba149282609aa0af43
Matt Aimonetti <mattaimonetti@gmail.com>
2017-12-03 17:28:31 -0800

    implement more of the Buffer interface

--- pcm_buffer.go
+++ pcm_buffer.go
@@ -1,5 +1,7 @@
 package audio
 
+import "math"
+
 // PCMDataFormat is an enum type to indicate the underlying data format used.
 type PCMDataFormat uint8
 
@@ -42,6 +44,9 @@ type PCMBuffer struct {
 	// The consumer of the buffer might want to look at this value to know what store
 	// to use to optimaly retrieve data.
 	DataType PCMDataFormat
+	// SourceBitDepth helps us know if the source was encoded on
+	// 1 (int8), 2 (int16), 3(int24), 4(int32), 8(int64) bytes.
+	SourceBitDepth uint8
 }
 
 // Len returns the length of the underlying data.
@@ -91,8 +96,17 @@ func (b *PCMBuffer) AsFloatBuffer() *FloatBuffer {
 	panic("not implemented")
 }
 
+// AsFloat32Buffer implements the Buffer interface and returns a float 32 version of itself.
 func (b *PCMBuffer) AsFloat32Buffer() *Float32Buffer {
-	panic("not implemented")
+	newB := &Float32Buffer{}
+	newB.Data = b.AsF32()
+	if b.Format != nil {
+		newB.Format = &Format{
+			NumChannels: b.Format.NumChannels,
+			SampleRate:  b.Format.SampleRate,
+		}
+	}
+	return newB
 }
 
 func (b *PCMBuffer) AsIntBuffer() *IntBuffer {
@@ -201,6 +215,86 @@ func (b *PCMBuffer) AsI32() (out []int32) {
 	return out
 }
 
+// AsF32 returns the buffer's samples as float32 sample values.
+// If the buffer isn't in this format, a copy is created and converted.
+// Note that converting might result in unexpected truncations.
+func (b *PCMBuffer) AsF32() (out []float32) {
+	if b == nil {
+		return nil
+	}
+	switch b.DataType {
+	case DataTypeI8:
+		bitDepth := b.calculateIntBitDepth()
+		factor := math.Pow(2, 8*float64(bitDepth/8)-1)
+		out = make([]float32, len(b.I8))
+		for i := 0; i < len(b.I8); i++ {
+			out[i] = float32(float64(int64(b.I8[i])) / factor)
+		}
+	case DataTypeI16:
+		bitDepth := b.calculateIntBitDepth()
+		factor := math.Pow(2, 8*float64(bitDepth/8)-1)
+		out = make([]float32, len(b.I16))
+		for i := 0; i < len(b.I16); i++ {
+			out[i] = float32(float64(int64(b.I16[i])) / factor)
+		}
+	case DataTypeI32:
+		bitDepth := b.calculateIntBitDepth()
+		factor := math.Pow(2, 8*float64(bitDepth/8)-1)
+		out = make([]float32, len(b.I16))
+		for i := 0; i < len(b.I16); i++ {
+			out[i] = float32(float64(int64(b.I16[i])) / factor)
+		}
+	case DataTypeF32:
+		return b.F32
+	case DataTypeF64:
+		out = make([]float32, len(b.F64))
+		for i := 0; i < len(b.F64); i++ {
+			out[i] = float32(b.F64[i])
+		}
+	}
+	return out
+}
+
+// AsF64 returns the buffer's samples as float64 sample values.
+// If the buffer isn't in this format, a copy is created and converted.
+// Note that converting might result in unexpected truncations.
+func (b *PCMBuffer) AsF64() (out []float64) {
+	if b == nil {
+		return nil
+	}
+	switch b.DataType {
+	case DataTypeI8:
+		bitDepth := b.calculateIntBitDepth()
+		factor := math.Pow(2, 8*float64(bitDepth/8)-1)
+		out = make([]float64, len(b.I8))
+		for i := 0; i < len(b.I8); i++ {
+			out[i] = float64(int64(b.I8[i])) / factor
+		}
+	case DataTypeI16:
+		bitDepth := b.calculateIntBitDepth()
+		factor := math.Pow(2, 8*float64(bitDepth/8)-1)
+		out = make([]float64, len(b.I16))
+		for i := 0; i < len(b.I16); i++ {
+			out[i] = float64(int64(b.I16[i])) / factor
+		}
+	case DataTypeI32:
+		bitDepth := b.calculateIntBitDepth()
+		factor := math.Pow(2, 8*float64(bitDepth/8)-1)
+		out = make([]float64, len(b.I16))
+		for i := 0; i < len(b.I16); i++ {
+			out[i] = float64(int64(b.I16[i])) / factor
+		}
+	case DataTypeF32:
+		out = make([]float64, len(b.F32))
+		for i := 0; i < len(b.F32); i++ {
+			out[i] = float64(b.F32[i])
+		}
+	case DataTypeF64:
+		return b.F64
+	}
+	return out
+}
+
 // Clone creates a clean clone that can be modified without
 // changing the source buffer.
 func (b *PCMBuffer) Clone() Buffer {
@@ -242,14 +336,14 @@ func (b *PCMBuffer) SwitchPrimaryType(t PCMDataFormat) {
 	}
 	switch t {
 	case DataTypeI8:
-		b.I8 = nil
+		b.I8 = b.AsI8()
 		b.I16 = nil
 		b.I32 = nil
 		b.F32 = nil
 		b.F64 = nil
 	case DataTypeI16:
 		b.I8 = nil
-		b.I16 = nil
+		b.I16 = b.AsI16()
 		b.I32 = nil
 		b.F32 = nil
 		b.F64 = nil
@@ -263,15 +357,75 @@ func (b *PCMBuffer) SwitchPrimaryType(t PCMDataFormat) {
 		b.I8 = nil
 		b.I16 = nil
 		b.I32 = nil
-		b.F32 = nil
+		b.F32 = b.AsF32()
 		b.F64 = nil
 	case DataTypeF64:
 		b.I8 = nil
 		b.I16 = nil
 		b.I32 = nil
 		b.F32 = nil
-		b.F64 = nil
+		b.F64 = b.AsF64()
 	}
 
 	b.DataType = t
+}
+
+// calculateIntBithDepth looks at the int values in the buffer and returns
+// the required lowest bit depth.
+func (b *PCMBuffer) calculateIntBitDepth() uint8 {
+	if b == nil {
+		return 0
+	}
+	bitDepth := b.SourceBitDepth
+	if bitDepth != 0 {
+		return bitDepth
+	}
+	var max int
+	switch b.DataType {
+	case DataTypeI8:
+		var i8max int8
+		for _, s := range b.I8 {
+			if s > i8max {
+				i8max = s
+			}
+		}
+		max = int(i8max)
+	case DataTypeI16:
+		var i16max int16
+		for _, s := range b.I16 {
+			if s > i16max {
+				i16max = s
+			}
+		}
+		max = int(i16max)
+	case DataTypeI32:
+		var i32max int32
+		for _, s := range b.I32 {
+			if s > i32max {
+				i32max = s
+			}
+		}
+		max = int(i32max)
+	default:
+		// This method is only meant to be used on int buffers.
+		return bitDepth
+	}
+	bitDepth = 8
+	if max > 127 {
+		bitDepth = 16
+	}
+	// greater than int16, expecting int24
+	if max > 32767 {
+		bitDepth = 24
+	}
+	// int 32
+	if max > 8388607 {
+		bitDepth = 32
+	}
+	// int 64
+	if max > 4294967295 {
+		bitDepth = 64
+	}
+
+	return bitDepth
 }
